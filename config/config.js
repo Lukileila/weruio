5d9328eb546980c9415f98822fd452514051fff9
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-09-20 23:27:44 +0200

    added config

---
+++ config/config.js
@@ -0,0 +1,19 @@
+const fontFamily = document.querySelector("#font-family");
+const form = document.querySelector("form");
+const configBody = document.querySelector("#configbody");
+
+if (!localStorage.getItem("font-family")) {localStorage.setItem("font-family","initial")}
+
+configBody.style.fontFamily = localStorage.getItem("font-family");
+
+fontFamily.value = "okkkk";
+fontFamily.value = localStorage.getItem("font-family");
+
+console.log("anything");
+
+form.addEventListener("submit", () => {
+    console.log("formsubmit called");
+    localStorage.setItem("font-family",fontFamily.value);
+    configBody.style.fontFamily = localStorage.getItem("font-family");
+    document
+    }); 
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const textarea = document.querySelector("textarea");
+
 textarea.value = localStorage.getItem("text");
+textarea.style.fontFamily = localStorage.getItem("font-family");
 
 const save = ()=>{
     localStorage.setItem("text",textarea.value)
